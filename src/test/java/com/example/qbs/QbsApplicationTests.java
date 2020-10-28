675e0d2dda5c2bd918ddbf99e57f7e7b11a91c92
Vitaliy Arkhanhelski <vit667@gmail.com>
2020-10-28 15:49:54 +0100

    works with strings

---
+++ src/test/java/com/example/qbs/QbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
